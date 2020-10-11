463f665100a62a4de655944e9cbe88ba819848ba
juninhopo <kelgaxd@gmail.com>
2020-10-10 23:50:44 -0400

    for

--- aulas/for1.js
+++ aulas/for1.js
@@ -7,8 +7,17 @@ while (contador <= 10) {
 
 }
 
+console.log(' ')
 console.log('contador com for')
 
 for (let i = 1; i < 10; i++) {
     console.log('contador = ' + i)
+}
+
+console.log(' ')
+console.log('navegando no array')
+
+const notas = [6.7, 5.5, 4.8, 8.5, 9.8]
+for (let i = 0; i < notas.length; i++) {
+    console.log('nota = ' + notas[i])
 }
