325a4a7490f31410f5bcae7cba7c749c36d2d47b
juninhopo <kelgaxd@gmail.com>
2020-10-10 23:32:40 -0400

    atualizando

--- aulas/whileComContador.js
+++ aulas/whileComContador.js
@@ -3,7 +3,7 @@ function getInteiroAleatorioEntre(min, max) {
     return Math.floor(valor)
 }
 
-let opcao = 0
+let opcao = 0   
 let vezes = 0 //para o contador de vezes passado iniciar em 0
 
 while (opcao != -1) {
