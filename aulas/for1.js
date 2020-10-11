5689a22c8c59cfc59a4b39103e23b39f3d0d0b32
juninhopo <kelgaxd@gmail.com>
2020-10-10 23:39:16 -0400

    for

--- aulas/for1.js
+++ aulas/for1.js
@@ -1,6 +1,14 @@
-let = contador1
+console.log('contador com while')
+
+let contador = 1
 while (contador <= 10) {
-    console.log('contador = ${contador}')
+    console.log('contador = ' + contador)
     contador++
-    
+
+}
+
+console.log('contador com for')
+
+for (let i = 1; i < 10; i++) {
+    console.log('contador = ' + i)
 }
