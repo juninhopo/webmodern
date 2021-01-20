62a505b37765f849bcbc5a636c61851620dec678
Darlan <kelgaxd@gmail.com>
2021-01-19 20:06:57 -0400

    update

--- js/breakcontinue.js
+++ js/breakcontinue.js
@@ -8,8 +8,17 @@ for (x in nums) {
 }
 
 for (y in nums) {
-    if(y == 5) {
-        continue 
+    if (y == 5) {
+        continue
     }
     console.log(`${y} = ${nums[y]}`)
 }
+
+externo: for (a in nums) {
+    for (b in nums) {
+        if (a == 2 && b == 3) break externo
+        console.log(`Par = ${a}, ${b}`)
+    }
+}
+
+console.log('Fim!')
