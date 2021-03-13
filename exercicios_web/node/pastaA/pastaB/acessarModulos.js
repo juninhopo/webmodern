640365a5d8f72508bc9d24b927d6685be4846ce5
Darlan Jr <kelgaxd@gmail.com>
2021-03-13 14:38:49 -0400

    update

---
+++ exercicios_web/node/pastaA/pastaB/acessarModulos.js
@@ -0,0 +1,12 @@
+const moduloA = require('../../modulo-A')
+
+console.log(moduloA.bemVindo)
+
+const c = require('./pastaC/index')
+console.log(c.ola2)
+
+/* const http = require('http')
+http.createServer((req, res) => {
+    res.write('Welcome!')
+    res.end()
+}).listen(8080) */
