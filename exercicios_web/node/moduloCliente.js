6ede27d915c6bd253154c3d491e4b8a902c4f03d
Darlan Jr <darlan.junior@stone.com.br>
2021-03-13 11:55:53 -0400

    update

---
+++ exercicios_web/node/moduloCliente.js
@@ -0,0 +1,12 @@
+const moduleA = require('./modulo-A')
+const moduleB = require('./modulo-B')
+
+
+console.log(moduleA.ola)
+console.log(moduleA.bemVindo)
+console.log(moduleA.ateLogo)
+console.log(moduleA)
+
+console.log(moduleB.bomDia)
+console.log(moduleB.boaNoite())
+console.log(moduleB)
